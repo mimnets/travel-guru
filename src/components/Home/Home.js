27bd113ee901b702537a4ffd84315808b8b46d66
mimnets <mdmonirulislammonir@gmail.com>
2020-09-22 23:41:36 +0600

    room shows successfully

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,6 +10,8 @@ import TourLocation from '../TourLocation/TourLocation';
         imgUrl: 'https://1.bp.blogspot.com/-pL1m3ZRemoE/X2drzEUAdyI/AAAAAAAAoGQ/3h6yh-0hTZMeSbfz-5QwyzkhyKpLlcsOwCLcBGAsYHQ/s320/CoxsBazar.png',
         roomUrl: 'https://1.bp.blogspot.com/-2yX6J8pFLg4/X2ntFw7bxyI/AAAAAAAAoGs/zT5BiyLNkcI21jDs7-23HvRVCTHmX93AwCLcBGAsYHQ/s320/Rectangle%2B26.png',
         rating: '4 star',
+        roomDesc:"Light bright airy style apt safe & peaceful stay",
+        roomSpec:"4 guests 2 beadrooms 2 beds 2 baths Wi-Fi air conditioning Kitchen",
         price: 54
     },
     {
@@ -19,6 +21,8 @@ import TourLocation from '../TourLocation/TourLocation';
       imgUrl: 'https://1.bp.blogspot.com/-c_jGTT0ahAs/X2drpcdTXyI/AAAAAAAAoGM/2ZqEGuKQk6kmKpz7L4NbVRIE3jqhz-d6QCLcBGAsYHQ/s320/Sreemongol.png',
       roomUrl:'https://1.bp.blogspot.com/-v6PCWL7EnyA/X2ntF-2HBjI/AAAAAAAAoGo/IOnMbCh0hxAJYb3_67ufN5WahLpwOD4IgCLcBGAsYHQ/s320/Rectangle%2B27.png',
       rating: '4.5 star',
+      roomDesc:"AR lounge and pool (R + R & B + B)",
+      roomSpec:"4 guests 2 beadrooms 2 beds 2 baths Wi-Fi air conditioning Kitchen",
       price: 59
   },
   {
@@ -28,6 +32,8 @@ import TourLocation from '../TourLocation/TourLocation';
       imgUrl: 'https://1.bp.blogspot.com/-2q1wOOsl5n8/X2dro6h3N2I/AAAAAAAAoGI/3FXcy0TVv44gVbtkMIICra9VHiO8S9JDwCLcBGAsYHQ/s320/Sundorbon.png',
       roomUrl:'https://1.bp.blogspot.com/-guDIlcmOb9M/X2ntF44X9iI/AAAAAAAAoGw/hf0sk__Z7K8jK_ZTLSlqJoRoEsU46JTUACLcBGAsYHQ/s320/Rectangle%2B28.png',
       rating: '4.7 star',
+      roomDesc:"Apartment in lost panaroma",
+      roomSpec:"4 guests 2 beadrooms 2 beds 2 baths Wi-Fi air conditioning Kitchen",
       price: 60
   }
   ]
