694340a1d86f216a3516eea219911f58d14b853e
mimnets <mdmonirulislammonir@gmail.com>
2020-10-27 17:52:10 +0600

    fix login error

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,7 @@
 import React, { useContext, useState } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
 import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,9 +10,8 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { FormControl, FormHelperText, Grid, Input, InputLabel } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import {loginFrameworkInitialization, handleGoogleSignIn, handleFBSignIn, createUserWithEmailAndPass, signInWithEmailAndPass} from './LoginInfo';
+import { Grid } from '@material-ui/core';
+import {handleGoogleSignIn, loginFrameworkInitialization} from './LoginInfo';
 
 const useStyles = makeStyles({
     root: {
@@ -42,135 +44,37 @@ const Login = () => {
     });
 
     loginFrameworkInitialization();
-
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const history = useHistory();
     const location = useLocation();
     let { from } = location.state || { from: { pathname: "/" } };
-
-    const googleSignIn =() => {
+    
+    const googleSignIn = () => {
         handleGoogleSignIn()
         .then(res => {
-            handleResponse(res, true); // Required for login redirects
-        })
+            handleResponse(res,true); // Required for login redirects
+          })
     }
 
-    // handleResponse need to declare as below :
-    // handleResponse start
-    const handleResponse = (res, redirect) =>{
+    const handleResponse = (res, redirect) => {
         setUser(res);
         setLoggedInUser(res);
         if(redirect){
             history.replace(from);
         }
-    }
-    // handleResponse end
-
-    const fbSignIn = () => {
-        handleFBSignIn()
-        .then(res => {
-            handleResponse(res,true);
-        })
-    }
-
-    const handleBlur = (event) => {
-        const newUserInfo = {...user};
-        newUserInfo[event.target.name] = event.target.value;
-        setUser(newUserInfo);
-    }
-
-    const handleSubmit = (event) => {
-        console.log(user.email,user.password);
-        if(newUser && user.email && user.password){
-            createUserWithEmailAndPass(user.name, user.email, user.password)
-            .then(res => {
-            handleResponse(res, true);
-        })
-        }
-        if(!newUser && user.email && user.password){
-            signInWithEmailAndPass(user.email, user.password)
-            .then(res => {
-            handleResponse(res, true);
-        })
-        }
-    event.preventDefault(); 
-    }
-
+      }
     return (
 
         <div style={{display:'flex', justifyContent:'center'}}>
             
                 <Card className={classes.root}>
                     <CardContent>
-                        <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id=""/>
-                        <label htmlFor="newUser">New User Sign Up</label>
-                        <form action="" onSubmit={handleSubmit}>
-                            {newUser && <input name="name" type="text" placeholder="Your name" />}
-                            <br/>
-                            <input name="email" type="text" onBlur={handleBlur}placeholder="Your email" required/>
-                            <br/>
-                            <input name="password" type="password" onBlur={handleBlur}placeholder="Your password" required/>
-                            <br/>
-                            <input type="submit" value={newUser ? 'Sign Up' : 'Sign In'}/>
-                        </form>
-                    {/* <FormControl>
-  <InputLabel htmlFor="my-input">Email address</InputLabel>
-  <Input id="my-input" aria-describedby="my-helper-text" />
-  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-</FormControl>
-                    <form action="" onSubmit={handleSubmit}>
-                    <CardContent>
-                    
-                    {newUser && <TextField
-                        id="outlined-disabled"
-                        label="Name"
-                        defaultValue=""
-                        variant="outlined"
-                        />
-                    }
-                        </CardContent>
-                        <CardContent>
-                        <TextField
-                        id="outlined-disabled"
-                        label="Email"
-                        defaultValue=""
-                        variant="outlined"
-                        />
-                        </CardContent>
-                        
-                        <CardContent>
-                        <TextField
-                        id="outlined-disabled"
-                        label="Password"
-                        defaultValue=""
-                        variant="outlined"
-                        />
-                        </CardContent>
-                        <CardContent>
-                        <Button>Submit</Button>
-                        </CardContent>
-                        </form> */}
-                        {/* <Typography className={classes.title} color="textSecondary" gutterBottom>
-                            Word of the Day
-                            </Typography>
-                        <Typography variant="h5" component="h2">
-
-                        </Typography>
-                        <Typography className={classes.pos} color="textSecondary">
-                            adjective
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            well meaning and kindly.
-                        </Typography> */}
                         <Typography variant="body2" component="p">
                         <Button onClick={googleSignIn}>Google Sign in</Button>
-                        <br/>
-                        <Button onClick={fbSignIn}>Facebook Sign in</Button>
                         </Typography>
                     </CardContent>
                 </Card>
-
-
+                
         </div>
     );
 };
